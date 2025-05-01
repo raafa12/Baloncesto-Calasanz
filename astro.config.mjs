9e4cc6946bd9f37c863355b449c4bd68d0cd6aee
raafa12 <rafabasanta@hotmail.com>
2025-05-01 17:51:50 +0200

    footerlink

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: {
-    session: true,
-  },
   output: 'server',
   adapter: netlify({
     edgeMiddleware: true
