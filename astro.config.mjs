27e5dfe6e43347d9f0297b4a9c0026a0df2f6a88
raafa12 <rafabasanta@hotmail.com>
2025-05-05 18:58:33 +0200

    cosis5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from '@astrojs/tailwind'; // ✅
-
+import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: netlify({
     edgeMiddleware: true
   }),
+  experimental: {
+    session: true
+  }
 });
