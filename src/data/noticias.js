f6e235a4798684d11c6b0f1567e065f1e8bbf902
raafa12 <rafabasanta@hotmail.com>
2025-07-01 16:05:05 +0200

    news37

--- src/data/noticias.js
+++ src/data/noticias.js
@@ -240,7 +240,7 @@ export const noticias = [
               <svg class="h-5 w-5 text-green-400 mr-2 mt-0.5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
-              <span>Para más información, contactar con el club en: <a href="mailto:clubcalasanzbaloncesto@gmail.com" class="text-green-300 hover:underline">info@baloncalcetacalasanz.com</a></span>
+              <span>Para más información, contactar con el club en: <a href="mailto:clubcalasanzbaloncesto@gmail.com" class="text-green-300 hover:underline">clubcalasanzbaloncesto@gmail.com</a></span>
             </li>
           </ul>
         </div>
